0bb505648b0a6fe66c051e52f43ee4f41fe54fc7
Mario Castelo <91093467@g.mvusd.net>
2021-01-31 01:36:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class Main
+{
+  public static void main(String args[])
+  {
+    JFrame frame1 = new JFrame("App");
+    frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame1.setSize(500, 500);
+    frame1.setVisible(true);
+
+  }
+}
